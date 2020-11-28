0013064844b04e267568a465d88fededfcae66c2
Adrian Hirt <aedu.hirt@gmail.com>
2020-11-28 20:17:57 +0100

    Add flash messages helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def flash_messages(opts = {})
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "alert alert-#{msg_type}", role: "alert") do 
+              concat content_tag(:button, 'x', class: 'close', data: { dismiss: 'alert' })
+              concat message 
+            end)
+    end
+    nil
+  end
 end
