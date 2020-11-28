39906ddc7bfebb694fdce9f89af8cc6eba13181b
Adrian Hirt <aedu.hirt@gmail.com>
2020-11-28 21:25:44 +0100

    Update flashes for bootstrap 5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
-      concat(content_tag(:div, message, class: "alert alert-#{msg_type}", role: "alert") do 
-              concat content_tag(:button, 'x', class: 'close', data: { dismiss: 'alert' })
+      concat(content_tag(:div, message, class: "alert alert-#{msg_type} alert-dismissible fade show", role: "alert") do 
+              concat content_tag(:button, nil, class: 'btn-close', data: { dismiss: 'alert' })
               concat message 
             end)
     end
