68c3bf9856056b8e9f2b9e9e203195a0a29e3fd7
Adrian Hirt <aedu.hirt@gmail.com>
2020-12-20 17:46:49 +0100

    Change error on cancancan not authorized for now to head 403

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     respond_to do |format|
       format.json { head :forbidden, content_type: 'text/html' }
-      format.html { redirect_to main_app.root_url, notice: exception.message }
+      format.html { head :forbidden }
       format.js   { head :forbidden, content_type: 'text/html' }
     end
   end
