7cc512c2e6da8c71ece767c47d351da9157dc09a
Adrian Hirt <aedu.hirt@gmail.com>
2020-12-20 22:21:45 +0100

    Adding some flashes

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -14,8 +14,10 @@ module Admin
 
     def create
       if run Operations::Admin::User::Create
+        flash[:success] = _('User|Successfully created')
         redirect_to admin_users_path
       else
+        flash[:danger] = _('User|Create failed')
         render 'new'
       end
     end
@@ -26,15 +28,19 @@ module Admin
 
     def update
       if run Operations::Admin::User::Update
+        flash[:success] = _('User|Successfully updated')
         redirect_to admin_users_path
       else
+        flash[:danger] = _('User|Update failed')
         render 'edit'
       end
     end
 
     def destroy
       if run Operations::Admin::User::Destroy
-
+        flash[:success] = _('User|Successfully deleted')
+      else
+        flash[:danger] = _('User|Cannot be deleted')
       end
       redirect_to admin_users_path
     end
