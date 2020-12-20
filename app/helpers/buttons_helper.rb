62dc7e0e47792bfb583fd38f74544282dd583061
Adrian Hirt <aedu.hirt@gmail.com>
2020-12-20 22:18:06 +0100

    extend button helper

--- app/helpers/buttons_helper.rb
+++ app/helpers/buttons_helper.rb
@@ -1,46 +1,60 @@
 module ButtonsHelper
-  def button(title, href, color: :primary, size: nil, outline: false, **link_opts)
-    classes = get_btn_class(color, outline, size)
-    _button(title, href, classes, link_opts)
+  DEFAULT_OPTS = {
+    namespace: [],
+    size:      nil,
+    color:     :primary,
+    outline:   false,
+    icon_only: false,
+    disabled:  false
+  }.freeze
+
+  def button(title, href, html: {}, btn_icon: nil, **opts)
+    options = get_options(opts)
+    if btn_icon
+      button_icon = icon btn_icon
+      title = (options[:icon_only] ? button_icon : (button_icon + title))
+    end
+    _button(title, href, get_btn_class(options), **html)
   end
 
-  def show_button(model, href: nil, namespace: [], color: :primary, size: nil, outline: false, icon_only: false, disabled: false, **link_opts)
-    href ||= polymorphic_path(namespace << model)
-    classes = get_btn_class(:primary, outline, size, icon_only, disabled)
+  def show_button(model, html: {}, **opts)
+    options = get_options(opts)
+    href = options[:href] || polymorphic_path(options[:namespace] << model)
     title = icon %i[fa fa-arrow-right]
-    title += _("#{model.class.name}|Show") unless icon_only
-    _button(title, href, classes, link_opts)
+    title += _("#{model.class.name}|Show") unless options[:icon_only]
+    _button(title, href, get_btn_class(options), **html)
   end
 
-  def new_button(klass, href: nil, namespace: [], color: :primary, size: nil, outline: false, icon_only: false, disabled: false, **link_opts)
-    href ||= new_polymorphic_path(namespace << klass)
-    classes = get_btn_class(:primary, outline, size, icon_only, disabled)
+  def new_button(klass, html: {}, **opts)
+    options = get_options(opts)
+    href = options[:href] || new_polymorphic_path(options[:namespace] << klass)
     title = icon %i[fa fa-plus]
-    title += _("#{klass.name}|New") unless icon_only
-    _button(title, href, classes, link_opts)
+    title += _("#{klass.name}|New") unless options[:icon_only]
+    _button(title, href, get_btn_class(options), **html)
   end
 
-  def edit_button(model, href: nil, namespace: [], color: :primary, size: nil, outline: false, icon_only: false, disabled: false, **link_opts)
-    href ||= edit_polymorphic_path(namespace << model)
-    classes = get_btn_class(:primary, outline, size, icon_only, disabled)
+  def edit_button(model, html: {}, **opts)
+    options = get_options(opts)
+    href = options[:href] || edit_polymorphic_path(options[:namespace] << model)
     title = icon %i[fa fa-pencil]
-    title += _("#{model.class.name}|Edit") unless icon_only
-    _button(title, href, classes, link_opts)
+    title += _("#{model.class.name}|Edit") unless options[:icon_only]
+    _button(title, href, get_btn_class(options), **html)
   end
 
-  def delete_button(model, href: nil, namespace: [], color: :primary, size: nil, outline: false, icon_only: false, disabled: false, **link_opts)
-    href ||= polymorphic_path(namespace << model)
-    classes = get_btn_class(:danger, outline, size, icon_only, disabled)
+  def delete_button(model, html: {}, **opts)
+    opts[:color] ||= :danger
+    options = get_options(opts)
+    href = options[:href] || polymorphic_path(options[:namespace] << model)
     title = icon %i[fa fa-trash]
-    title += _("#{model.class.name}|Delete") unless icon_only
-    options = {
+    title += _("#{model.class.name}|Delete") unless options[:icon_only]
+    html_options = {
       method: :delete,
       data:   {
         confirm: _("#{model.class.name}|Delete confirmation?")
       }
     }
-    link_opts.merge!(options)
-    _button(title, href, classes, link_opts)
+    html_options.merge!(html)
+    _button(title, href, get_btn_class(options), **html_options)
   end
 
   private
@@ -51,12 +65,16 @@ module ButtonsHelper
     end
   end
 
-  def get_btn_class(color, outline, size, icon_only, disabled)
+  def get_btn_class(**opts)
     classes = %i[btn]
-    classes << (outline ? "btn-outline-#{color}" : "btn-#{color}")
-    classes << "btn-#{size}" if size
-    classes << 'icon-btn' unless icon_only
-    classes << 'disabled' if disabled
+    classes << (opts[:outline] ? "btn-outline-#{opts[:color]}" : "btn-#{opts[:color]}")
+    classes << "btn-#{opts[:size]}" if opts[:size]
+    classes << 'icon-btn' unless opts[:icon_only]
+    classes << 'disabled' if opts[:disabled]
     classes
+  end
+
+  def get_options(**opts)
+    DEFAULT_OPTS.merge(opts)
   end
 end
