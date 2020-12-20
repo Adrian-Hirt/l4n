2a10de50a5bdc46844422bc1aabd66720bcc1883
Adrian Hirt <aedu.hirt@gmail.com>
2020-12-20 21:47:23 +0100

    Fix tooltip js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
 import 'bootstrap';
 
@@ -20,4 +20,4 @@ import 'bootstrap';
 document.addEventListener("turbolinks:load", () => {
   $('[data-toggle="tooltip"]').tooltip()
   $('[data-toggle="popover"]').popover()
-})
+});
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,15 @@
-const { environment } = require('@rails/webpacker')
- 
-const webpack = require('webpack')
-environment.plugins.append('Provide',
+const { environment } = require('@rails/webpacker');
+const customConfig = require('./custom');
+ const webpack = require('webpack');
+
+ environment.plugins.append('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery',
     Popper: ['popper.js', 'default']
   })
-)
+);
+
+environment.config.merge(customConfig);
  
-module.exports = environment
+module.exports = environment;
