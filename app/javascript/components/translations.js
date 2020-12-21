c19ecdabe77f2667c94045c4221ee3d5da14b808
Adrian Hirt <aedu.hirt@gmail.com>
2020-12-21 19:58:00 +0100

    Use Locales of i18n in Rails for JS translations

--- app/javascript/components/translations.js
+++ app/javascript/components/translations.js
@@ -1,11 +1,15 @@
 const gettext = require('gettext.js');
 export const i18n = gettext.default();
 
-const languages = ['en']; // Array of locales
+import {available_locales} from 'components/locales.js.erb';
+
+i18n._ = function(str) {
+  return i18n.gettext(str);
+}
 
 export default class Translations {
   static loadTranslations() {
-    for(let lang of languages) {
+    for(let lang of available_locales) {
       let json_data = require('../locale/' + lang + '.json');
       i18n.loadJSON(json_data, 'messages');
     }
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,5 @@
 const { environment } = require('@rails/webpacker');
+const erb = require('./loaders/erb')
 const customConfig = require('./custom');
  const webpack = require('webpack');
 
@@ -12,4 +13,5 @@ const customConfig = require('./custom');
 
 environment.config.merge(customConfig);
  
+environment.loaders.prepend('erb', erb)
 module.exports = environment;
