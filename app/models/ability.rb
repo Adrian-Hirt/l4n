c368ff3e32c279c6dd9429f3d05a83957559b81a
Adrian Hirt <aedu.hirt@gmail.com>
2021-02-06 14:07:29 +0100

    Add missing ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,11 +2,11 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # rubocop:disable Style/GuardClause
     if user
       can :manage, :all
       can :access, :admin_panel
     end
-    # rubocop:enable Style/GuardClause
+
+    can :read, NewsPost
   end
 end
