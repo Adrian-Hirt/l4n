30f9fefc2abb68d76614bcfdb7a5fbb8167d4bc3
Adrian Hirt <aedu.hirt@gmail.com>
2021-02-06 14:06:25 +0100

    Require stronger password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   has_secure_password :remember_me_token, validations: false
 
   validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password, presence: true, length: { minimum: 8 }, confirmation: true, if: -> { password.present? || new_record? }
+  validates :password, presence: true, length: { minimum: 10, maximum: 72 }, confirmation: true, if: -> { password.present? || new_record? }
 end
