16fa4d1a4a1c9b389aa67a7b92e452e7af32e28f
Adrian Hirt <aedu.hirt@gmail.com>
2021-04-14 17:06:24 +0200

    Fix alertsi [ci skip]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def flash_messages(_opts = {})
     flash.each do |msg_type, message|
       concat(content_tag(:div, message, class: "alert alert-#{msg_type} alert-dismissible fade show notification-flash", role: 'alert') do
-               concat content_tag(:button, nil, class: 'btn-close', data: { 'bs-dismiss': 'alert' })
+               concat content_tag(:button, '<span aria-hidden="true">&times;</span>'.html_safe, class: 'close', data: { 'dismiss': 'alert' })
                concat message
              end)
     end
