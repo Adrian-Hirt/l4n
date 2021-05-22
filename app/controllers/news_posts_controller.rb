a15f0cae94ae4b280cadbab3e796f0bbf87078e0
Adrian Hirt <aedu.hirt@gmail.com>
2021-05-22 17:47:24 +0200

    Only pass page param to news post index op [ci skip]

--- app/controllers/news_posts_controller.rb
+++ app/controllers/news_posts_controller.rb
@@ -1,6 +1,6 @@
 class NewsPostsController < ApplicationController
   def index
-    op Operations::NewsPost::Index
+    op Operations::NewsPost::Index, page: params.dig(:page)
   end
 
   def show
--- app/operations/news_post/index.rb
+++ app/operations/news_post/index.rb
@@ -3,6 +3,10 @@ module Operations::NewsPost
     # No auth needed
     without_authorization
 
+    schema3 do
+      str? :page
+    end
+
     def posts
       # TODO: When pagination is added, put into query class
       ::NewsPost.where(published: true).order(:published_at).page(osparams.page)
