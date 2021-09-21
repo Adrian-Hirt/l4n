05ac063eb002e907b5edc8e5905434cfec4b94fe
Adrian Hirt <aedu.hirt@gmail.com>
2021-09-21 17:17:08 +0200

    Test with lcov formatter [ci skip]

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,8 @@
 require 'simplecov'
+require 'simplecov-lcov'
+
+SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
 SimpleCov.start 'rails' do
   enable_coverage :branch
 
