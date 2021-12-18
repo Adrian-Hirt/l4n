847f55bbc8c0301ae095f81fc0c19b033c3b30a5
Adrian Hirt <aedu.hirt@gmail.com>
2021-12-18 01:08:56 +0100

    Comply with rubocop

--- app/grids/users.rb
+++ app/grids/users.rb
@@ -16,10 +16,10 @@ module Grids
     column :'datagrid-actions', html: true, header: '' do |user|
       tag.div class: %i[datagrid-actions-wrapper] do
         safe_join([
-          show_button(user, namespace: %i[admin], size: :sm, icon_only: true),
-          edit_button(user, href: profile_admin_user_path(user), size: :sm, icon_only: true),
-          delete_button(user, namespace: %i[admin], size: :sm, icon_only: true, disabled: user == current_user)
-        ])
+                    show_button(user, namespace: %i[admin], size: :sm, icon_only: true),
+                    edit_button(user, href: profile_admin_user_path(user), size: :sm, icon_only: true),
+                    delete_button(user, namespace: %i[admin], size: :sm, icon_only: true, disabled: user == current_user)
+                  ])
       end
     end
   end
