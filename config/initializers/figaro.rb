10e933092fc810757541f4b7b55bc237c626532b
Adrian Hirt <hirta@student.ethz.ch>
2021-12-26 16:16:34 +0100

    Skip required keys if env var present

--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -1,4 +1,4 @@
-unless Rails.env.test?
+unless Rails.env.test? || ENV['RUNNING_ON_HEROKU'].present?
   Figaro.require_keys(%w[
                         hcaptcha_site_key
                         hcaptcha_site_secret
