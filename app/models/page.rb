4f1d90a98667fb14dc8539c2388e705796d12a1b
Adrian Hirt <aedu.hirt@gmail.com>
2022-01-02 20:00:21 +0100

    Also destroy menuitems when a page is destroyed

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,9 +1,26 @@
 class Page < ApplicationRecord
+  ################################### Attributes ###################################
+
+  ################################### Constants ####################################
+
+  ################################### Associations #################################
+  has_many :menu_items, dependent: :destroy
+
+  ################################### Validations ##################################
   validates :title, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 255 }
   validates :url, presence: true, uniqueness: { case_sensitive: false }, format: { with: /\A[a-z0-9]{1}[a-z0-9\-_]*\z/ }, length: { maximum: 255 }
   validates :published, inclusion: [true, false]
 
+  ################################### Hooks #######################################
+
+  ################################### Scopes #######################################
+
+  ################################### Class Methods ################################
+
+  ################################### Instance Methods #############################
   def readable?
     published? && FeatureFlag.enabled?(:pages)
   end
+
+  ################################### Private Methods ##############################
 end
