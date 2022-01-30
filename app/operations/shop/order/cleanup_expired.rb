bd346c68aaa0e31cf4a7edc74777990c5aba8b42
Adrian Hirt <aedu.hirt@gmail.com>
2022-01-30 16:51:38 +0100

    Put queries into query classes for easier testing

--- app/operations/shop/order/cleanup_expired.rb
+++ app/operations/shop/order/cleanup_expired.rb
@@ -6,12 +6,12 @@ module Operations::Shop::Order
       # Delete exired orders
       ActiveRecord::Base.transaction do
         # Remove expired created orders
-        Order.where(status: Order.statuses[:created]).where('cleanup_timestamp < ?', Time.zone.now - ::Order::TIMEOUT).each do |order|
+        Queries::Order::FetchCreatedExpired.run.each do |order|
           Operations::Shop::Order::CleanupSingleOrder.run order: order
         end
 
         # Remove expired payment pending orders
-        Order.where(status: Order.statuses[:payment_pending]).where('cleanup_timestamp < ?', Time.zone.now - ::Order::TIMEOUT_PAYMENT_PENDING).each do |order|
+        Queries::Order::FetchPaymentPendingExpired.run.each do |order|
           Operations::Shop::Order::CleanupSingleOrder.run order: order
         end
       end
