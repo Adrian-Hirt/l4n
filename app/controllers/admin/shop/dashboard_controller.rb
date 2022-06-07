11211f81d5071862bf6c794a252cb2fee807b365
Adrian Hirt <aedu.hirt@gmail.com>
2022-06-07 22:19:46 +0200

    Add shop dashboard to sidenav

--- app/controllers/admin/shop/dashboard_controller.rb
+++ app/controllers/admin/shop/dashboard_controller.rb
@@ -1,7 +1,7 @@
 module Admin
   module Shop
     class DashboardController < AdminController
-      add_breadcrumb _('Admin|Dashboard'), :admin_shop_dashboard_path
+      add_breadcrumb _('Admin|Shop|Dashboard'), :admin_shop_dashboard_path
 
       def index; end
     end
