9a3e2f7f2daa010f91c20ad5d82639c7260f62cc
Adrian Hirt <aedu.hirt@gmail.com>
2022-06-15 20:09:09 +0200

    Filter promotion codes

--- app/grids/admin/promotion_codes.rb
+++ app/grids/admin/promotion_codes.rb
@@ -2,16 +2,25 @@ module Grids
   module Admin
     class PromotionCodes < ApplicationGrid
       scope do
-        PromotionCode.order(:id)
+        PromotionCode.includes(promotion_code_mapping: :order).order(:id)
       end
 
       model PromotionCode
 
+      SELECT_OPTIONS = [
+        [_('PromotionCodes|Show only used'), true],
+        [_('PromotionCodes|Show only unused'), false],
+        [_('PromotionCodes|Show all'), nil]
+      ]
+
       column :code
-      column :used, html: true do |promotion_code|
+      column :used, html: true, order: false do |promotion_code|
+        # TODO: this somehow produces many queries, despite the includes
         format_boolean(promotion_code.order.present?)
       end
+
       column :used_for_order, html: true do |promotion_code|
+        # TODO: this somehow produces many queries, despite the includes
         link_to(promotion_code.order.formatted_id, admin_shop_order_path(promotion_code.order)) if promotion_code.order.present?
       end
       # column :'datagrid-actions', html: true, header: false do |promotion_code|
@@ -23,6 +32,18 @@ module Grids
       # end
 
       filter(:promotion)
+
+      filter(:used, :enum, select: SELECT_OPTIONS, include_blank: false) do |value, scope|
+        scope = scope.joins('LEFT OUTER JOIN promotion_code_mappings ON promotion_codes.id = promotion_code_mappings.promotion_code_id')
+
+        value = value.blank? ? nil : ::Datagrid::Utils.booleanize(value)
+
+        if value
+          scope = scope.where('promotion_code_mappings.id IS NOT NULL')
+        else
+          scope = scope.where('promotion_code_mappings.id IS NULL')
+        end
+      end
     end
   end
 end
