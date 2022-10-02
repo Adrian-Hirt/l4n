35e510578f06318e66f6e6689aaa81483332724b
Adrian Hirt <aedu.hirt@gmail.com>
2022-10-02 17:25:57 +0200

    Remove unused link

--- app/grids/admin/seat_categories.rb
+++ app/grids/admin/seat_categories.rb
@@ -15,7 +15,6 @@ module Grids
       column :'datagrid-actions', html: true, header: false do |seat_category|
         tag.div class: %i[datagrid-actions-wrapper] do
           safe_join([
-                      show_button(seat_category, namespace: %i[admin], size: :sm, icon_only: true),
                       edit_button(seat_category, namespace: %i[admin], size: :sm, icon_only: true),
                       delete_button(seat_category, namespace: %i[admin], size: :sm, icon_only: true)
                     ])
