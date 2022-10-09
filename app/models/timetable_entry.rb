95a751419c8c51c00c57e8c3b3de6780e8a79d73
Adrian Hirt <aedu.hirt@gmail.com>
2022-10-09 18:55:54 +0200

    TimetableEntry: end needs to be at last 1 hour after start

--- app/models/timetable_entry.rb
+++ app/models/timetable_entry.rb
@@ -11,6 +11,7 @@ class TimetableEntry < ApplicationRecord
   validates :link, length: { maximum: 255 }
   validates :entry_start, presence: true
   validates :entry_end, presence: true, comparison: { greater_than: :entry_start }
+  validate :entry_end_sensible
 
   # == Hooks =======================================================================
 
@@ -21,4 +22,17 @@ class TimetableEntry < ApplicationRecord
   # == Instance Methods ============================================================
 
   # == Private Methods =============================================================
+  private
+
+  # We want to have at least 1 hour between the start and the end (such that it's
+  # properly rendered in the timetable).
+  def entry_end_sensible
+    # Don't do anything if either of the datetimes is not present
+    return if entry_start.blank? || entry_end.blank?
+
+    # Don't do anything if the datetimes are at least one hour apart
+    return if entry_end - entry_start >= 1.hour
+
+    errors.add(:entry_end, 'TimetableEntry|End should be at least 1 hour after start')
+  end
 end
