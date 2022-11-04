4fb249d50c79d29780595ca52c31296b705f54f1
Adrian Hirt <aedu.hirt@gmail.com>
2022-11-04 21:22:27 +0100

    Next events sidebar

--- app/operations/application/sidebar.rb
+++ app/operations/application/sidebar.rb
@@ -2,12 +2,18 @@ module Operations::Application
   class Sidebar < RailsOps::Operation
     schema3 {} # No params here
 
-    def first_block
-      'This is the first block of the sidebar'
+    def lan_party_block
+      return unless FeatureFlag.enabled?(:lan_party)
+
+      ApplicationController.render partial: 'shared/sidebar/lan_party_info', locals: { lan_party: LanParty.active } if LanParty.active.present?
     end
 
-    def lan_party_block
-      ApplicationController.render partial: 'shared/sidebar/lan_party_info', locals: { lan_party: LanParty.active } if LanParty.active.present?
+    def next_events_block
+      return unless FeatureFlag.enabled?(:events)
+
+      next_relevant_events = Queries::Event::FetchFutureEvents.call.first(5)
+
+      ApplicationController.render partial: 'shared/sidebar/next_events', locals: { events: next_relevant_events } if next_relevant_events.any?
     end
   end
 end
