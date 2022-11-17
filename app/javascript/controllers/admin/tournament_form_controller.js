b654444eba8c7b48c84e2f00eb7005093c95e649
Adrian Hirt <aedu.hirt@gmail.com>
2022-11-17 21:43:45 +0100

    Display tournament files in frontend

--- app/javascript/controllers/admin/tournament_form_controller.js
+++ app/javascript/controllers/admin/tournament_form_controller.js
@@ -4,20 +4,22 @@ export default class extends Controller {
   static targets = ['participantType', 'teamSize'];
 
   connect() {
-    var radios = this.participantTypeTarget.querySelectorAll('input[type=radio]');
-    this.teamSizeInput = this.teamSizeTarget.querySelector('input');
+    if (this.hasParticipantTypeTarget) {
+      var radios = this.participantTypeTarget.querySelectorAll('input[type=radio]');
+      this.teamSizeInput = this.teamSizeTarget.querySelector('input');
 
-    for(let radio of radios) {
-      radio.addEventListener('change', (e) => {
-        if(e.target.value == 'true') {
-          this.teamSizeInput.disabled = true;
-          this.teamSizeTarget.classList.add('d-none');
-        }
-        else {
-          this.teamSizeInput.disabled = false;
-          this.teamSizeTarget.classList.remove('d-none');
-        }
-      });
+      for(let radio of radios) {
+        radio.addEventListener('change', (e) => {
+          if(e.target.value == 'true') {
+            this.teamSizeInput.disabled = true;
+            this.teamSizeTarget.classList.add('d-none');
+          }
+          else {
+            this.teamSizeInput.disabled = false;
+            this.teamSizeTarget.classList.remove('d-none');
+          }
+        });
+      }
     }
   }
 }
