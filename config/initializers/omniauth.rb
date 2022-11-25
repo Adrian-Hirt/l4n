dcfab6c990729ce5114fc4b83dd12e8034928ecd
Adrian Hirt <aedu.hirt@gmail.com>
2022-11-25 17:38:25 +0100

    Comply with rubocop

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,12 +2,8 @@ unless Rails.env.test? || Figaro.env.building_docker_image
 
   # Only configure the providers if the relevant secrets are set
   Rails.application.config.middleware.use OmniAuth::Builder do
-    if Figaro.env.steam_web_api_key.present?
-      provider :steam, Figaro.env.steam_web_api_key
-    end
+    provider :steam, Figaro.env.steam_web_api_key if Figaro.env.steam_web_api_key.present?
 
-    if Figaro.env.discord_id.present? && Figaro.env.discord_secret.present?
-      provider :discord, Figaro.env.discord_id!, Figaro.env.discord_secret!
-    end
+    provider :discord, Figaro.env.discord_id!, Figaro.env.discord_secret! if Figaro.env.discord_id.present? && Figaro.env.discord_secret.present?
   end
 end
