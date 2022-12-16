9bd0197211a4c781cc6ae5746a4051247c365c3e
Adrian Hirt <aedu.hirt@gmail.com>
2022-12-16 18:52:48 +0100

    Add filtering of shop by categories

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,7 @@
 ### Changed
 
 * Moved `location` from `Event` to `EventDate` model
+* Add filtering of shop by categories
 * Allow dots and slashes in page URL
 * Disable password autofill in registration and update profile form
 * Improve styling of markdown rendering
--- app/operations/shop/home/index.rb
+++ app/operations/shop/home/index.rb
@@ -1,18 +1,31 @@
 module Operations::Shop::Home
   class Index < RailsOps::Operation
-    schema3 {} # No params allowed for now
+    schema3 do
+      int? :category_id, cast_str: true
+    end
 
     policy :on_init do
       authorize! :read, :shop
     end
 
     def products
-      @products ||= ::Product.where(on_sale: true)
-                             .joins(:product_category)
-                             .order('product_categories.sort' => :asc)
-                             .order(sort: :asc)
-                             .where.associated(:product_variants)
-                             .includes(:product_category, :product_variants)
+      @products ||= begin
+        relevant_products = ::Product.where(on_sale: true)
+
+        if osparams.category_id.present?
+          relevant_products = relevant_products.where(product_category_id: osparams.category_id)
+        else
+          relevant_products = relevant_products.joins(:product_category).order('product_categories.sort' => :asc)
+        end
+
+        relevant_products.order(sort: :asc)
+                         .where.associated(:product_variants)
+                         .includes(:product_category, :product_variants)
+      end
+    end
+
+    def available_categories
+      @available_categories ||= ::ProductCategory.where(id: ::Product.where(on_sale: true).map(&:id)).order(:name)
     end
   end
 end
