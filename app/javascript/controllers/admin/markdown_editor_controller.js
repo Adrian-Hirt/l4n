06ba785d50311a7c39888620d4668e4c4b188374
Adrian Hirt <aedu.hirt@gmail.com>
2022-12-16 15:34:36 +0100

    Fix markdown editor duplicating

--- app/javascript/controllers/admin/markdown_editor_controller.js
+++ app/javascript/controllers/admin/markdown_editor_controller.js
@@ -3,8 +3,22 @@ import MarkdownEditor from '../../components/markdown_editor'
 
 export default class extends Controller {
   connect() {
+    this.#removeOldEditor();
+
     setTimeout(() => {
       new MarkdownEditor(this.element);
     }, 100);
+  }
+
+  #removeOldEditor() {
+    let nextSibling = this.element.nextElementSibling;
+
+    if(!nextSibling) {
+      return;
+    }
+
+    if(nextSibling.classList.contains('EasyMDEContainer')) {
+      nextSibling.remove();
+    }
   }
 }
