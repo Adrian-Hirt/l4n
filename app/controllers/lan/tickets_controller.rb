bbb404ac38701cca5be1a21f88779df3016fa9b0
Adrian Hirt <aedu.hirt@gmail.com>
2022-12-26 15:08:29 +0100

    Fix flashes shown twice

--- app/controllers/lan/tickets_controller.rb
+++ app/controllers/lan/tickets_controller.rb
@@ -2,53 +2,53 @@ module Lan
   class TicketsController < LanController
     def assign
       if run Operations::Ticket::AssignToUser
-        flash[:success] = _('Ticket|Successfully assigned')
+        flash.now[:success] = _('Ticket|Successfully assigned')
       else
-        flash[:danger] = _('Ticket|Could not be assigned')
+        flash.now[:danger] = _('Ticket|Could not be assigned')
       end
 
       respond_to :turbo_stream
     rescue Operations::Exceptions::OpFailed => e
-      flash[:danger] = e.message
+      flash.now[:danger] = e.message
       respond_to :turbo_stream
     end
 
     def remove_assignee
       if run Operations::Ticket::RemoveAssignee
-        flash[:success] = _('Ticket|Successfully removed assignee')
+        flash.now[:success] = _('Ticket|Successfully removed assignee')
       else
-        flash[:danger] = _('Ticket|Assignee could not be removed')
+        flash.now[:danger] = _('Ticket|Assignee could not be removed')
       end
 
       respond_to :turbo_stream
     rescue Operations::Exceptions::OpFailed => e
-      flash[:danger] = e.message
+      flash.now[:danger] = e.message
       respond_to :turbo_stream
     end
 
     def take_seat
       if run Operations::Ticket::TakeSeat
-        flash[:success] = _('Ticket|Successfully taken seat')
+        flash.now[:success] = _('Ticket|Successfully taken seat')
       else
-        flash[:danger] = _('Ticket|Seat could not be taken')
+        flash.now[:danger] = _('Ticket|Seat could not be taken')
       end
 
       respond_to :turbo_stream
     rescue Operations::Exceptions::OpFailed => e
-      flash[:danger] = e.message
+      flash.now[:danger] = e.message
       respond_to :turbo_stream
     end
 
     def remove_seat
       if run Operations::Ticket::RemoveSeat
-        flash[:success] = _('Ticket|Successfully removed seat')
+        flash.now[:success] = _('Ticket|Successfully removed seat')
       else
-        flash[:danger] = _('Ticket|Seat could not be removed')
+        flash.now[:danger] = _('Ticket|Seat could not be removed')
       end
 
       respond_to :turbo_stream
     rescue Operations::Exceptions::OpFailed => e
-      flash[:danger] = e.message
+      flash.now[:danger] = e.message
       respond_to :turbo_stream
     end
 
