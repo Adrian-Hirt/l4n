30f8c20f108d698b9e349e7ece24ec36d03b2572
Adrian Hirt <aedu.hirt@gmail.com>
2023-06-10 12:00:10 +0200

    Remove unused constants

--- app/models/session.rb
+++ app/models/session.rb
@@ -3,9 +3,6 @@ class Session < ActiveRecord::SessionStore::Session
 
   # == Constants ===================================================================
   EXPIRATION_TIME = 1.day
-  REMEMBER_ME_USER_COOKIE = '_l4n_user_id'.freeze
-  REMEMBER_ME_TOKEN_COOKIE = '_l4n_remember_token'.freeze
-  REMEMBER_ME_TOKEN_EXPIRES_AFTER = 2.weeks
 
   # == Associations ================================================================
 
