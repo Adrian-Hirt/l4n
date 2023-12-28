0f5f5f0d902a86850babebebad2aefcdbabe107d
Adrian Hirt <aedu.hirt@gmail.com>
2023-12-28 12:34:35 +0100

    Comply with rubocop

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module L4n
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
 
     # Override layouts for Doorkeeper
     config.to_prepare do
